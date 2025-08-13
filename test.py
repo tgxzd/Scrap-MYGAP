83f8503e35c30e91b1b1e7bfd4b2547e32237337
Atan0707 <harizhakim85@gmail.com>
2025-08-13 12:42:09 +0800

    push venv & update requirement

--- test.py
+++ test.py
@@ -1,8 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
 
-response = requests.get('https://carianmygapmyorganic.doa.gov.my/mygap_pf_list.php')
+# 1. Get the page
+response = requests.get('https://carianmygapmyorganic.doa.gov.my/mygap_pf_list.php?pagesize=500')
 
 soup = BeautifulSoup(response.content, 'html.parser')
 
 print(soup.prettify())
+
+# 
